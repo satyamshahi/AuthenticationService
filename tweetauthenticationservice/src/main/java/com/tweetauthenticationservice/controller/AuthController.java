539b2d2184dc0253814b55a18452f017f11d30d6
Smriti <smriti8647@gmail.com>
2022-06-14 16:05:14 +0530

    added cross orgion

--- tweetauthenticationservice/src/main/java/com/tweetauthenticationservice/controller/AuthController.java
+++ tweetauthenticationservice/src/main/java/com/tweetauthenticationservice/controller/AuthController.java
@@ -18,6 +18,7 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,6 +41,7 @@ import com.tweetauthenticationservice.security.service.RegistrationService;
  */
 @RestController
 @RequestMapping("/api/v1.0/tweets")
+@CrossOrigin(origins="http://localhost:4200")
 public class AuthController {
 
 	private final AuthenticationManager authManager;
